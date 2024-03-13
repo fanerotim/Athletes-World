09a7e108252c8bbc1f80266ac184d14a7e9d3dbb
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-13 19:49:16 +0000

    set up dashboard component and connected it to router

--- athletes-world/src/app/app-routing.module.ts
+++ athletes-world/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: HomeComponent}
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
--- athletes-world/src/app/main/main.module.ts
+++ athletes-world/src/app/main/main.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule
