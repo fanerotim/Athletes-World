2f59d192331bb1da219818f5b3b7fbc2c4e5b4e2
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-13 14:56:27 +0000

    created core module and added header and footer component

--- athletes-world/src/app/app.module.ts
+++ athletes-world/src/app/app.module.ts
@@ -10,9 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
