1c59638770da580df5b25860a0176359accde778
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-18 17:48:18 +0000

    changed name of API service

--- athletes-world/src/app/main/dashboard/dashboard.component.ts
+++ athletes-world/src/app/main/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiServiceService } from '../api-service.service';
+import { ApiServiceService } from '../api.service';
 
 @Component({
   selector: 'app-dashboard',
