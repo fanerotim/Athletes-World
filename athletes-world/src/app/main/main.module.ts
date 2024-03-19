87689feed5a0764c2823ee19d6cf183a1dfdf313
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-19 16:34:37 +0000

    configured the new routing to work in main module

--- athletes-world/src/app/main/main.module.ts
+++ athletes-world/src/app/main/main.module.ts
@@ -4,18 +4,21 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http'
 import { SharedModule } from '../shared/shared.module';
-
+import { CurrentAthleteComponent } from './current-athlete/current-athlete.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    CurrentAthleteComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class MainModule { }
