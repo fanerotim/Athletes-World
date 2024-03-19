1173de7dc99970c5cd167628cc26277f7417b422
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-19 16:32:34 +0000

    fixed naming of the API service

--- athletes-world/src/app/main/api.service.ts
+++ athletes-world/src/app/main/api.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Athelete } from './types/Athlete';
+import { Athlete } from './types/Athlete';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService {
+export class ApiService {
 
   constructor(private http: HttpClient) { }
 
   getAthletes() {
-    return this.http.get<Athelete[]>('http://localhost:3030/athletes')
+    return this.http.get<Athlete[]>('http://localhost:3030/athletes')
+  }
+
+  getOne(id: string) {
+    console.log(id);
+    return this.http.get<Athlete>(`http://localhost:3030/athletes/${id}`)
   }
 }
