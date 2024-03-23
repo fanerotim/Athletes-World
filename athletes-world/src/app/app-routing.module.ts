363734d52b5ffa925054f7d830a096feb27c3f4b
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-23 14:41:19 +0000

    set up auth guard for login and register

--- athletes-world/src/app/app-routing.module.ts
+++ athletes-world/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './main/home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 import { NotFoundComponent } from './main/not-found/not-found.component';
+import { AuthGuard } from './auth.guard';
+import { CanActivate } from '@angular/router';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'login', canActivate: [AuthGuard], component: LoginComponent},
+  {path: 'register', canActivate: [AuthGuard], component: RegisterComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
---
+++ athletes-world/src/app/auth.guard.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { BehaviorSubject, EMPTY, Observable } from "rxjs";
+import { UserService } from "./user/user.service";
+import { UserForAuth } from "./user/types/User";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private userService: UserService) {}
+
+    
+    isAuthenticated$$ = new BehaviorSubject<UserForAuth | boolean >(true);    
+    
+    canActivate(
+        route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot): 
+        boolean | 
+        UrlTree | 
+        Observable<boolean | 
+        UrlTree> | 
+        Promise<boolean | 
+        UrlTree> {
+        return this.isAuth(state.url) || this.router.createUrlTree(['/404'])
+    }
+
+    isAuth(url: string) {
+        this.userService.getProfile().subscribe(data => this.isAuthenticated$$.next(!!data));
+    
+
+        if (this.isAuthenticated$$.value) {
+            return false
+        } else {
+            return true
+        }
+    }
+}
