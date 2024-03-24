949a6601da25eeadd2c5b4be5c92d87326f9e59e
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-24 12:36:30 +0000

    set up edit form and corrected general form styling

--- athletes-world/src/app/main/edit/edit.component.ts
+++ athletes-world/src/app/main/edit/edit.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService,
+    private fb: FormBuilder) { }
+
+  editForm = this.fb.group({
+    name: [''],
+    age: [''],
+    country: [''],
+    achievements: [''],
+    imgUrl: ['']
+})
+
+
+    ngOnInit(): void {
+
+      this.activatedRoute.params.subscribe(data => {
+
+        let athleteId = data['athleteId'];
+
+        this.apiService.getOne(athleteId).subscribe(athlete => {
+          console.log(athlete);
+        })
+      })
+    }
+
+    handleEdit() {
+      
+    }
 }
