13c0ce49262f67c1ab666edf540bd722510366f5
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-24 10:31:31 +0000

    set up routing for edit

--- athletes-world/src/app/main/athlete-routing.module.ts
+++ athletes-world/src/app/main/athlete-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { CurrentAthleteComponent } from "./current-athlete/current-athlete.component";
 import { CreateComponent } from "./create/create.component";
+import { EditComponent } from "./edit/edit.component";
 
 const routes: Routes = [
     {path: 'athletes', children: [
         {path: '', pathMatch: 'full', component: DashboardComponent},
         {path: 'create', component: CreateComponent},
-        {path: ':athleteId', component: CurrentAthleteComponent}
+        {path: ':athleteId', component: CurrentAthleteComponent},
+        {path: ':athleteId/edit', component: EditComponent}
     ]},
 ]
 
--- athletes-world/src/app/main/current-athlete/current-athlete.component.ts
+++ athletes-world/src/app/main/current-athlete/current-athlete.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Athlete } from '../types/Athlete';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-current-athlete',
@@ -12,20 +13,32 @@ export class CurrentAthleteComponent implements OnInit {
 
   constructor(
     private apiService: ApiService,
-    private activatedRoute: ActivatedRoute
-    ) {}
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {}
 
   athleteDetails = {} as Athlete;
 
+  athleteId: string = '';
+
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(data => {
-      const athleteId = data['athleteId'];
       
+      const athleteId = data['athleteId'];
+      this.athleteId = athleteId;
+
       const athleteInfo = this.apiService
       .getOne(athleteId)
       .subscribe(data => {
         this.athleteDetails = data;
       })
     })
+  }
+
+  handleEdit() {
+    this.router.navigate([`athletes/${this.athleteId}/edit`])
+  }
+
+  handleDelete() {
+
   }
 }
