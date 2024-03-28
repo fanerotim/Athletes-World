5d656d5c570ff7597b51601710561bc6f56e770d
Petar Cholakov <petar.cholakov30@gmail.com>
2024-03-28 17:13:07 +0000

    set up initial profile page styling

--- athletes-world/src/app/user/profile/profile.component.ts
+++ athletes-world/src/app/user/profile/profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { UserForAuth } from '../types/User';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  userInfo = {} as UserForAuth;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+
+    this.userService.getProfile().subscribe(data => {
+      this.userInfo = data;
+      console.log(this.userInfo);
+    })
+  }
 }
